ccf184982780bc799cddfd3d17d20f3c2623fc2a
ClementTvs <clementtravers010@gmail.com>
2026-06-11 14:45:37 +0200

    fix: player controller not working

--- ComfyJamSummer/Source/ComfyJamSummer/Private/MyPlayerController.cpp
+++ ComfyJamSummer/Source/ComfyJamSummer/Private/MyPlayerController.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MyPlayerController.h"
 #include "Ingredients.h"
 
@@ -28,22 +27,19 @@ void AMyPlayerController::OnClickPressed()
 {
     FHitResult Hit;
     GetHitResultUnderCursor(ECC_Visibility, false, Hit);
-
     if (Hit.GetActor())
-	{
-		if (Hit.GetComponent()->GetName() == TEXT("HitBox"))
-		{
-			SelectedActor = Hit.GetActor();
+    {
+        SelectedActor = Hit.GetActor();
 
-			if (AIngredients* Ingredient = Cast<AIngredients>(SelectedActor))
-				Ingredient->OnGrabbed();
+        if (AIngredients* Ingredient = Cast<AIngredients>(SelectedActor))
+        {
+            Ingredient->OnGrabbed();
+        }
 
-			FVector WorldLocation, WorldDirection;
-			if (DeprojectMousePositionToWorld(WorldLocation, WorldDirection))
-				GrabOffset = SelectedActor->GetActorLocation() - WorldLocation;
-
-		}
-	}
+        FVector WorldLocation, WorldDirection;
+        if (DeprojectMousePositionToWorld(WorldLocation, WorldDirection))
+            GrabOffset = SelectedActor->GetActorLocation() - WorldLocation;
+    }
 }
 
 void AMyPlayerController::OnClickReleased()
@@ -58,18 +54,13 @@ void AMyPlayerController::Tick(float DeltaTime)
     Super::Tick(DeltaTime);
     if (!SelectedActor)
         return;
-
     FVector WorldLocation;
     FVector WorldDirection;
     if (DeprojectMousePositionToWorld(WorldLocation, WorldDirection))
     {
-        AMoveableSprite *moveableActor = Cast<AMoveableSprite>(SelectedActor);
-        if(moveableActor)
-        {
-            FVector NewLocation = moveableActor->GetMoveable()->GetComponentLocation();
-       		NewLocation.X = WorldLocation.X + GrabOffset.X;
-        	NewLocation.Z = WorldLocation.Z + GrabOffset.Z;
-            moveableActor->GetMoveable()->SetWorldLocation(NewLocation);
-        } 
+        FVector NewLocation = SelectedActor->GetActorLocation();
+        NewLocation.X = WorldLocation.X + GrabOffset.X;
+        NewLocation.Z = WorldLocation.Z + GrabOffset.Z;
+        SelectedActor->SetActorLocation(NewLocation);
     }
 }
